2d13c6edb218d86c0631ee26440a45e4c92ea7ce
Geordan Krahn <geordankrahn@gmail.com>
2022-01-14 16:24:31 -0700

    Add project files.

---
+++ CelAnimationSequence.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Lab01
+{
+    /// <summary>
+    /// Represents a cel animated texture.
+    /// </summary>
+    public class CelAnimationSequence
+    {
+        // The texture containing the animation sequence...
+        protected Texture2D texture;
+
+        // The length of time a cel is displayed...
+        protected float celTime;
+
+        // Sequence metrics
+        protected int celWidth;
+        protected int celHeight;
+
+        // Calculated count of cels in the sequence
+        public int celCountX { get; set; }
+        public int celCountY { get; set; } // added in for the extra rows
+
+        /// <summary>
+        /// Constructs a new CelAnimationSequence.
+        /// </summary>        
+        public CelAnimationSequence(Texture2D texture, int celWidth, float celTime)
+        {
+            this.texture = texture;
+            this.celWidth = celWidth;
+            this.celTime = celTime;
+
+            celHeight = Texture.Height;
+            celCountX = Texture.Width / celWidth;
+            celCountY = Texture.Height / celHeight;
+        }
+
+        /// <summary>
+        /// All frames in the animation arranged horizontally.
+        /// </summary>
+        public Texture2D Texture
+        {
+            get { return texture; }
+        }
+
+        /// <summary>
+        /// Duration of time to show each cel.
+        /// </summary>
+        public float CelTime
+        {
+            get { return celTime; }
+        }
+
+        /// <summary>
+        /// Gets the number of cels in the animation.
+        /// </summary>
+        public int CelCount
+        {
+            get { return celCountX; }
+        }
+
+        /// <summary>
+        /// Gets the width of a frame in the animation.
+        /// </summary>
+        public int CelWidth
+        {
+            get { return celWidth; }
+        }
+
+        /// <summary>
+        /// Gets the height of a frame in the animation.
+        /// </summary>
+        public int CelHeight
+        {
+            get { return celHeight; }
+        }
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lab01
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        // This will be used to draw the idle sprite or animate the sprite sheet at a point.
+        Vector2 spritePosition;
+
+        // These images will fill the scene.
+        Texture2D backgroundImage;
+        Texture2D idleSprite; // for our character.
+        CelAnimationSequence walking;
+        CelAnimationPlayer playerAnimator;
+
+        // for the boundaries
+        float screenHeight;
+        float screenWidth;
+        float floorHeight; // our character needs to "walk" on this
+
+        // states to keep track of our character
+        MovementState currentState;
+        FacingDirection direction;
+
+        // State for whether we are moving or not.
+        enum MovementState
+        {
+            Idle = 0,
+            Walking = 1
+        }
+
+        // Need a facing direction. If facing right, keep sprite orientation
+        // If facing left flip the image - -1 scale should work.
+        enum FacingDirection
+        {
+            Right = 0,
+            Left = 1
+        }
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //Texture2D spriteSheet = 
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // This will keep track of and update the states and position of our character and listen for input.
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            // This will draw the scene each frame. Draw either the idle state or walking state and animation based on whether we are moving
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+    }
+}
