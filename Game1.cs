6735c0caa7e536976130cae275c8db424661a93d
Geordan Krahn <geordankrahn@gmail.com>
2022-01-14 22:05:18 -0700

    took the cel animation classes out of the player class. made members private, created accessors and mutators, made CaptureInput public, updated the constructor.

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace Project1
 {
@@ -11,6 +10,11 @@ namespace Project1
         private Texture2D backgroundImage;
         const int CEL_WIDTH = 172; // The dimentions of our cell
         // const int CEL_HEIGHT = 171; this is determined by the animation classes
+
+        private Texture2D idleSprite; // - PLAYER
+        private CelAnimationSequence walking; // - PLAYER
+        private CelAnimationPlayer playerAnimator; // - PLAYER
+
         const float CEL_TIME = 1 / 8.0f;
         const int SCREEN_HEIGHT = 224; // for the boundaries
         const int SCREEN_WIDTH = 384; // for the boundaries
@@ -25,10 +29,8 @@ namespace Project1
 
         protected override void Initialize()
         {
-            Texture2D idleSprite = Content.Load<Texture2D>("megaman-idle");
-            Texture2D spriteSheet = Content.Load<Texture2D>("mega-man-sprite-sheet");
-            CelAnimationSequence sequence = new CelAnimationSequence(spriteSheet, CEL_WIDTH, CEL_TIME);
-            player1 = new Player(new Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), 2, idleSprite, new CelAnimationPlayer(), sequence, FacingDirection.Right);
+            
+            player1 = new Player(new Vector2(0,0), 2, FacingDirection.Right);
             graphics.PreferredBackBufferWidth = SCREEN_WIDTH;
             graphics.PreferredBackBufferHeight = SCREEN_HEIGHT;
             graphics.ApplyChanges();
@@ -39,11 +41,18 @@ namespace Project1
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             backgroundImage = Content.Load<Texture2D>("background"); // set up the background image
+            idleSprite = Content.Load<Texture2D>("megaman-idle");
+            Texture2D spriteSheet = Content.Load<Texture2D>("mega-man-sprite-sheet");
+            walking = new CelAnimationSequence(spriteSheet, CEL_WIDTH, CEL_TIME);
+            // Set up the animation player
+            playerAnimator = new CelAnimationPlayer();
+            playerAnimator.Play(walking);
         }
 
         protected override void Update(GameTime gameTime)
         {
-            player1.Update(gameTime, SCREEN_WIDTH, SCREEN_HEIGHT);
+            UpdatePlayer();
+            playerAnimator.Update(gameTime); // this just queues the next frame of animation
             base.Update(gameTime);
         }
 
@@ -52,9 +61,66 @@ namespace Project1
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
             spriteBatch.Draw(backgroundImage, Vector2.Zero, Color.White); // draw background
-            player1.Draw(spriteBatch); // draw our character
+            DrawPlayer(); // draw our character
             spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        // This will keep track of and update the states and position of our character and listen for input.
+        public void UpdatePlayer()
+        {
+            player1.CaptureInput();
+            int horizontalPosition = (int)player1.Position().X;
+            int spriteWidth = idleSprite.Width;
+            // if we are out of the play area, reset the position 
+            if ( horizontalPosition > SCREEN_WIDTH - spriteWidth)
+            {
+                // We have gone too far too the right, reset position to just back inside
+                player1.UpdatePosition(new Vector2(SCREEN_WIDTH - spriteWidth - player1.HorizontalSpeed(), player1.Position().Y));
+            }
+            else if (horizontalPosition < spriteWidth)
+            {
+                player1.UpdatePosition(new Vector2(spriteWidth + player1.HorizontalSpeed(), player1.Position().Y));
+            }
+            else
+            {
+                Movement motionState = player1.Motion();
+                if (motionState == Movement.Walking)
+                {
+                    FacingDirection direction = player1.Direction();
+                    if (direction == FacingDirection.Right)
+                    {
+                        player1.UpdatePosition(new Vector2(horizontalPosition + player1.HorizontalSpeed(), player1.Position().Y));
+                    }
+                    else if (direction == FacingDirection.Left)
+                    {
+                        player1.UpdatePosition(new Vector2(horizontalPosition - player1.HorizontalSpeed(), player1.Position().Y));
+                    }
+                }
+            }
+        }
+
+        // This will draw the scene each frame. Draw either the idle state or walking state and animation based on whether we are moving
+        public void DrawPlayer()
+        {
+            Movement motionState = player1.Motion();
+            Vector2 spritePosition = player1.Position();
+            if (motionState == Movement.Idle)
+            {
+                spriteBatch.Draw(idleSprite, spritePosition, Color.White);
+            }
+            else if (motionState == Movement.Walking)
+            {
+                FacingDirection direction = player1.Direction();
+                if (direction == FacingDirection.Right)
+                {
+                    playerAnimator.Draw(spriteBatch, spritePosition, SpriteEffects.None);
+                }
+                else if (direction == FacingDirection.Left)
+                {
+                    playerAnimator.Draw(spriteBatch, spritePosition, SpriteEffects.FlipHorizontally);
+                }
+            }
         }
     }
 }
--- Player.cs
+++ Player.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace Project1
@@ -10,36 +6,64 @@ namespace Project1
     class Player
     {
         // This will be used to draw the idle sprite or animate the sprite sheet at a point.
-        private Vector2 spritePosition; // - PLAYER
+        private Vector2 spritePosition;
 
         // Horizontal Speed
-        private int horizontalSpeed; // - PLAYER
+        private int horizontalSpeed;
 
-        private Texture2D idleSprite; // - PLAYER
-        private CelAnimationSequence walking; // - PLAYER
-        private CelAnimationPlayer playerAnimator; // - PLAYER
-        
         // states to keep track of our character
-        Movement motionState; // - PLAYER
-        FacingDirection direction; // - PLAYER
+        private Movement motionState;
+        private FacingDirection direction;
 
         // Greedy Constructor
-        public Player(Vector2 spritePosition, int horizontalSpeed, Texture2D idleSprite, CelAnimationPlayer playerAnimator, CelAnimationSequence walking, FacingDirection initialFacingDirection)
+        public Player(Vector2 spritePosition, int horizontalSpeed, FacingDirection initialFacingDirection)
         {
             // initialize this player
             this.spritePosition = spritePosition;
             this.horizontalSpeed = horizontalSpeed;
-            this.idleSprite = idleSprite;
-            this.playerAnimator = playerAnimator;
-            this.walking = walking;
             direction = initialFacingDirection;
-
-            // Set up the animation player
-            playerAnimator = new CelAnimationPlayer();
-            playerAnimator.Play(walking);
+            motionState = Movement.Idle;
         }
 
-        private void CaptureInput()
+        // accessors
+        public void UpdatePosition(Vector2 newPosition)
+        {
+            spritePosition = newPosition;
+        }
+
+        public Vector2 Position()
+        {
+            return spritePosition;
+        }
+
+        public void UpdateHorizontalSpeed(int newSpeed)
+        {
+            horizontalSpeed = newSpeed;
+        }
+        public int HorizontalSpeed()
+        {
+            return horizontalSpeed;
+        }
+
+        public void UpdateMotionState(Movement newMotion)
+        {
+            motionState = newMotion;
+        }
+        public Movement Motion()
+        {
+            return motionState;
+        }
+
+        public void UpdateDirection(FacingDirection newDirection)
+        {
+            direction = newDirection;
+        }
+        public FacingDirection Direction()
+        {
+            return direction;
+        }
+
+        public void CaptureInput()
         {
             // Get the current keyboard state
             KeyboardState kbState = Keyboard.GetState();
@@ -58,59 +82,6 @@ namespace Project1
             {
                 motionState = Movement.Walking;
                 direction = FacingDirection.Right;
-            }
-        }
-
-        // This will keep track of and update the states and position of our character and listen for input.
-        public void Update(GameTime gameTime, int screenWidth, int screenHeight)
-        {
-            CaptureInput();
-
-            // if we are out of the play area, reset the position 
-            if (spritePosition.X > screenWidth - idleSprite.Width)
-            {
-                // We have gone too far too the right, reset position to just back inside
-                spritePosition.X = screenWidth - idleSprite.Width - horizontalSpeed;
-            }
-            else if (spritePosition.X < idleSprite.Width)
-            {
-                spritePosition.X = idleSprite.Width + horizontalSpeed;
-            }
-            else
-            {
-                if (motionState == Movement.Walking)
-                {
-                    if (direction == FacingDirection.Right)
-                    {
-                        spritePosition.X += horizontalSpeed;
-                    }
-                    else if (direction == FacingDirection.Left)
-                    {
-                        spritePosition.X += -horizontalSpeed;
-                    }
-                }
-            }
-
-            playerAnimator.Update(gameTime); // this just queues the next frame of animation
-        }
-
-        // This will draw the scene each frame. Draw either the idle state or walking state and animation based on whether we are moving
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            if (motionState == Movement.Idle)
-            {
-                spriteBatch.Draw(idleSprite, spritePosition, Color.White);
-            }
-            else if (motionState == Movement.Walking)
-            {
-                if (direction == FacingDirection.Right)
-                {
-                    playerAnimator.Draw(spriteBatch, spritePosition, SpriteEffects.None);
-                }
-                else if (direction == FacingDirection.Left)
-                {
-                    playerAnimator.Draw(spriteBatch, spritePosition, SpriteEffects.FlipHorizontally);
-                }
             }
         }
     }
